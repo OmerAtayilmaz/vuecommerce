2f8ce869a8521fcf73d769fa8785854c1d094036
Ömer Atayilmaz <64100323+OmerAtayilmaz@users.noreply.github.com>
2023-02-27 17:57:34 +0300

    Skeleton loader

---
+++ src/modules/products/product-skeleton/ProductSkeleton.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="col-md-4 col-lg-4 product-skeleton " >
+        <div class="product-skeleton__image p-2"></div>
+        <div class="product-skeleton__content p-2">
+        <div class="product-skeleton__title p-2"></div>
+        <div class="product-skeleton__price p-2"></div>
+        <div class="product-skeleton__description p-3"></div>
+        </div>
+    </div>
+</template>
+<style scoped>
+/* Skeleton loader css */
+.product-skeleton {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    padding: 1rem;
+    background-color: #fff;
+    border-radius: 0.5rem;
+    overflow: hidden;
+}
+.product-skeleton__image {
+    width: 400px;
+    height: 400px;
+    background-color: #f0f0f0;
+    border-radius: 0.5rem;
+    animation: skeleton-loader 1s infinite;
+}
+.product-skeleton__content {
+    width: 100%;
+    margin-top: 1rem;
+}
+.product-skeleton__title {
+    width: 100%;
+    height: 1.5rem;
+    background-color: #f0f0f0;
+    border-radius: 0.5rem;
+    animation: skeleton-loader 1s infinite;
+}
+.product-skeleton__price {
+    width: 100%;
+    height: 1.5rem;
+    margin-top: 1rem;
+    background-color: #f0f0f0;
+    border-radius: 0.5rem;
+    animation: skeleton-loader 1s infinite;
+}
+.product-skeleton__description {
+    width: 100%;
+    height: 1.5rem;
+    margin-top: 1rem;
+    background-color: #f0f0f0;
+    border-radius: 0.5rem;
+    animation: skeleton-loader 1s infinite;
+}
+@keyframes skeleton-loader {
+    0% {
+        background-color: #f0f0f0;
+    }
+    50% {
+        background-color: #e0e0e0;
+    }
+    100% {
+        background-color: #f0f0f0;
+    }
+}
+</style>
--- src/modules/products/single-product/SingleProduct.vue
+++ src/modules/products/single-product/SingleProduct.vue
@@ -1,5 +1,6 @@
 <template>
-<div class="col-md-4 col-lg-4 product ">
+<product-skeleton v-if="!isActive" />
+<div v-else  class="col-md-4 col-lg-4 product ">
     <div class="product--image">
         <img src="../../../../public/shoe.jpg" alt="product image" width="400" height="400" style="object-fit: cover;">
     </div>
@@ -11,10 +12,28 @@
 </div>
 </template>
 <script>
+import ProductSkeleton from "../product-skeleton/ProductSkeleton.vue";
+import {ref} from "vue";
+
+let isActive = ref(false); 
 export default{
+    
     name:"SingleProduct",
     props:{
         product:Object
+    },
+    components:{
+        ProductSkeleton
+    },
+    created(){
+        setTimeout(()=>{
+            isActive.value = ref(true);
+        }, 2000);
+    },
+    setup(){
+        return{
+            isActive
+        }
     }
 }
 </script>
